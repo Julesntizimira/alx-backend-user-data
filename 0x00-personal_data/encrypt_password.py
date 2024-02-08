56415f0b17f3384febe51112fb5cff7ac4502943
Julesntizimira <ntizimijules5@gmail.com>
2024-02-08 15:11:04 +0200

    Encrypting passwords and check valid password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Encrypting passwords
+    Check valid password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Encrypting passwords"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check valid password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
