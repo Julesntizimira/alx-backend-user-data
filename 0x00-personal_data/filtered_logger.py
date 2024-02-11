d18f5671fda0bc4761f402f9d932155f51fb1695
Julesntizimira <ntizimijules5@gmail.com>
2024-02-11 12:28:24 +0200

    revise Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,12 +27,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
-        '''init constructor method'''
+        """init constructor method"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        '''Log formatter'''
+        """Log formatter recods"""
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super().format(record)
