bcc9540faba443fa17e8a6135be6a9e3bc6a3798
Julesntizimira <ntizimijules5@gmail.com>
2024-02-12 15:12:51 +0200

    Base64 decode

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -9,7 +9,7 @@ class BasicAuth(Auth):
     '''BasicAuth class'''
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        '''Base64 encoding'''
+        '''basic Base64'''
         if not authorization_header:
             return None
         if not isinstance(authorization_header, str):
@@ -17,3 +17,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        '''Base64 decode'''
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            import base64
+            b64 = base64_authorization_header.encode("utf8")
+            encoded_utf8 = base64.b64decode(b64)
+            decoded_utf_str = encoded_utf8.decode("utf-8")
+        except Exception:
+            return None
+        return decoded_utf_str
