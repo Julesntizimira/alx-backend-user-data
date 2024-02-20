28f7b15c0a3680e08fd20602e2f1b5c13dd66465
Julesntizimira <ntizimijules5@gmail.com>
2024-02-20 10:09:37 +0200

    update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar, Any
+from typing import Union, Any, NoReturn
 from user import Base, User
 
 
@@ -37,7 +37,7 @@ class DB:
         self.__session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Any) -> User:
+    def find_user_by(self, **kwargs: Any) -> Union[User, NoReturn]:
         """
         takes in arbitrary keyword arguments and
         returns the first row found in the users
