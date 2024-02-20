bb3b91e9022cf440a9fcebe4c0d53e744182635c
Julesntizimira <ntizimijules5@gmail.com>
2024-02-20 09:36:56 +0200

    authentication_service

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,36 +1,15 @@
 #!/usr/bin/env python3
 """
- End-to-end integration test
+Main file
 """
-import json
-import requests
 
+from db import DB
+from user import User
 
-EMAIL = "guillaume@holberton.io"
-PASSWD = "b4l0u"
-NEW_PASSWD = "t4rt1fl3tt3"
+my_db = DB()
 
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
 
-def register_user(email, password):
-    '''assert Auth.register_user'''
-    form_data = {'email': email, 'password': password}
-    response = requests.post('http://0.0.0.0:5200/users', data=form_data)
-    resp = response.json()
-    if response.status_code == 200:
-        assert json.loads(resp) == {"email": email, "message": "user created"}
-    elif response.status_code == 400:
-        assert json.loads(resp) == {"message": "email already registered"}
-
-
-
-
-if __name__ == "__main__":
-    register_user(EMAIL, PASSWD)
-    '''log_in_wrong_password(EMAIL, NEW_PASSWD)
-    profile_unlogged()
-    session_id = log_in(EMAIL, PASSWD)
-    profile_logged(session_id)
-    log_out(session_id)
-    reset_token = reset_password_token(EMAIL)
-    update_password(EMAIL, reset_token, NEW_PASSWD)
-    log_in(EMAIL, NEW_PASSWD)'''
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
