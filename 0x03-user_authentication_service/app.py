a8fa5bbbbd9f0897ecd14ff6aa82a26a561083df
Julesntizimira <ntizimijules5@gmail.com>
2024-02-20 11:04:51 +0200

    update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,7 @@ def logout():
         abort(403)
     user = AUTH.get_user_from_session_id(session_id=session_id)
     if user:
-        AUTH.destroy_session(session_id)
+        AUTH.destroy_session(user.id)
         return redirect("/")
     abort(403)
 
