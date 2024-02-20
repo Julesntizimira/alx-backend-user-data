3ee40bd6b3176a74435af05e0eaf09e6150fc1fb
Julesntizimira <ntizimijules5@gmail.com>
2024-02-20 10:35:58 +0200

    update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -59,7 +59,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs: Dict[str, Any]) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         '''update user'''
         user = self.find_user_by(id=user_id)
         for key, val in kwargs.items():
