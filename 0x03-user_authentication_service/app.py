1335af896ef4f7d3ed558940a47fa0243b250e17
Julesntizimira <ntizimijules5@gmail.com>
2024-02-20 10:53:59 +0200

    update auth.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,7 +57,7 @@ def logout():
 @app.route('/profile', strict_slashes=False)
 def get_profile():
     '''get user profile'''
-    session_id = request.form.get('session_id')
+    session_id = request.cookies.get('session_id', None)
     user = AUTH.get_user_from_session_id(session_id=session_id)
     if user:
         return jsonify({"email": user.email}), 200
